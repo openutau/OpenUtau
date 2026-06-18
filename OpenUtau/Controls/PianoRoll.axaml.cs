c303e110c936437642c6b96b41289404c6af6f68
Maiko <maikotattun@yahoo.co.jp>
2026-06-19 01:04:00 +0900

    Improved the hit-test logic for overlap points

--- OpenUtau/Controls/PianoRoll.axaml.cs
+++ OpenUtau/Controls/PianoRoll.axaml.cs
@@ -1258,8 +1258,7 @@ namespace OpenUtau.App.Controls {
                 return;
             }
             var hitInfo = ViewModel.NotesViewModel.HitTest.HitTestPhoneme(point.Position);
-            var adjacent = hitInfo.phoneme != null && hitInfo.phoneme.Next != null && hitInfo.phoneme.Next.adjacent;
-            if (hitInfo.hitPosition || hitInfo.hitPreutter || (hitInfo.hitOverlap && adjacent) || hitInfo.hitAttackTime || hitInfo.hitReleaseTime) {
+            if (hitInfo.hitPosition || hitInfo.hitPreutter || hitInfo.hitOverlap || hitInfo.hitAttackTime || hitInfo.hitReleaseTime) {
                 Cursor = ViewConstants.cursorSizeWE;
                 ViewModel.MouseoverPhoneme(null);
                 return;
